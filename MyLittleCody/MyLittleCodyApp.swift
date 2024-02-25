fe556a65bcc37a1a2de1b2e57eb51aa7521c716c
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2024-02-25 21:28:57 +0300

    Initial Commit

---
+++ MyLittleCody/MyLittleCodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLittleCodyApp.swift
+//  MyLittleCody
+//
+//  Created by Николай Никитин on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyLittleCodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
